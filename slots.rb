d97025f8787d9ab86da14e94921b9711be3506aa
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-11 14:58:10 -0600

    fix under 20 sound

---
+++ slots.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require 'colorize'
+
+
+def welcome
+    puts "Welcome to Super Fun Slots!"
+    puts "Try your luck!"
+    pull
+end
+
+
+
+#print `say winner`
+#print `clear`
+
+
+
+def pull
+    image = ["cherry", "bar", "diamond", "dollar", "lemon", "orange"]
+    puts "#{image.sample}      #{image.sample}        #{image.sample}"
+when
+end    
+welcome
+
+#def muliply (p1, p2, p3)
+ # if p1 = p2 && p2 = p3:
+  #  multiplier = 3;
+
+  #else p1 = p2 || p2 = p3
+   # multiplier = 2;
+
+  #elsif  
+   # multiplier = 0;
+
+    #return multiplier
+  #end
+#end
--- wallet.rb
+++ wallet.rb
@@ -5,8 +5,15 @@ class Wallet
     @money = money
   end
 
+<<<<<<< HEAD
 
 
+=======
+<<<<<<< HEAD
+  # def money
+  #   x = @money
+  # end
+>>>>>>> fix under 20 sound
 
   # def increase(x)
   #   @wallet = x + 5
@@ -39,6 +46,33 @@ end
   #    @money += y
   #   binding.pry
   # end
+=======
+  def self.money
+    @x = @money
+  end
+
+
+  def self.increase
+    @money = @x + 5
+  end
+
+
+  def self.decrease
+    @money = @x - 5
+  if @money == 0
+    puts "Thanks for playing"
+
+    exit
+  else
+  end
+
+  def self.add_money
+    puts "How much would you like to add?"
+     y = gets.strip.to_i
+     @money = @x + y
+    binding.pry
+  end
+>>>>>>> fix under 20 sound
 
   # def decrease(x)
   #   @wallet = x - 5
