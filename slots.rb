c31224a2a005f6374ccedf5f834b15582ec496e3
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-11 15:59:29 -0600

    update sound

--- slots.rb
+++ slots.rb
@@ -1,10 +1,16 @@
 require 'pry'
 require 'colorize'
+require_relative 'wallet'
 
-
+#casino(wallet)
 def welcome
     puts "Welcome to Super Fun Slots!"
     puts "Try your luck!"
+    puts "What would you like to bet? "
+    bet = gets.input.i
+    puts "You have bet $#{bet}"
+    casino(wallet)
+    wallet = wallet - bet
     pull
 end
 
@@ -17,21 +23,30 @@ end
 
 def pull
     image = ["cherry", "bar", "diamond", "dollar", "lemon", "orange"]
-    puts "#{image.sample}      #{image.sample}        #{image.sample}"
-when
+    result1 = image.sample
+    result2 = image.sample
+    result3 = image.sample
+    puts "#{result1}      #{result2}        #{result3}"
+    puts `say "Congratulations you have won!!"`
+    end
+    
+
+def win (column1, column2, column3)
+     if column1 = column2 && column2 = column3
+        win = 3
+        puts `say "Congratulations you have won!!"`
+    
+     elsif column1 = column2 || column2 = column3
+        win = 2
+        puts `say "Congratulations you have won!!"`
+    
+     else 
+        win = 0
+        puts `say "Sorry you are not a winner"`
+    
+    winnings = bet * win(result1, result2, result3)
+
+    end
 end    
 welcome
 
-#def muliply (p1, p2, p3)
- # if p1 = p2 && p2 = p3:
-  #  multiplier = 3;
-
-  #else p1 = p2 || p2 = p3
-   # multiplier = 2;
-
-  #elsif  
-   # multiplier = 0;
-
-    #return multiplier
-  #end
-#end
