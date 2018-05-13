f559aa6b997d219d5af7a1bb018e1ebf571e0a0c
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-13 15:28:14 -0600

    update to slots

--- slots.rb
+++ slots.rb
@@ -2,51 +2,66 @@ require 'pry'
 require 'colorize'
 require_relative 'wallet'
 
-#casino(wallet)
+class Slots
+    attr_accessor :name, :age, :money
+
+    def intialize(person)
+        @person = person
+        puts @person.name
+        puts @person.money -2
+
 def welcome
     puts "Welcome to Super Fun Slots!"
     puts "Try your luck!"
-    puts "What would you like to bet? "
-    bet = gets.input.i
-    puts "You have bet $#{bet}"
-    casino(wallet)
-    wallet = wallet - bet
+    puts "Press ENTER to pull slot machine"
+    puts gets.strip
+    #puts "What would you like to bet? "
+    #bet = gets.strip.to_i
+    #puts "You have bet $#{bet}"
+    #casino(wallet)
+    #wallet = wallet - bet
     pull
 end
 
 
 
-#print `say winner`
-#print `clear`
-
-
-
 def pull
     image = ["cherry", "bar", "diamond", "dollar", "lemon", "orange"]
-    result1 = image.sample
-    result2 = image.sample
-    result3 = image.sample
-    puts "#{result1}      #{result2}        #{result3}"
-    puts `say "Congratulations you have won!!"`
-    end
+    @result1 = image.sample
+    @result2 = image.sample
+    @result3 = image.sample
+    puts "********************************************"
+    puts
+    puts "#{@result1}      #{@result2}        #{@result3}"
+    puts
+    puts "********************************************"
+    win
+end
     
 
-def win (column1, column2, column3)
-     if column1 = column2 && column2 = column3
-        win = 3
-        puts `say "Congratulations you have won!!"`
-    
-     elsif column1 = column2 || column2 = column3
-        win = 2
-        puts `say "Congratulations you have won!!"`
-    
-     else 
-        win = 0
-        puts `say "Sorry you are not a winner"`
-    
-    winnings = bet * win(result1, result2, result3)
+def win
+        # winner = (@result1 == @result2) && (@result2 == @result3)
+        # winner = (@result1 == @result2) && (@result2 == @result3)
+        # if winner == true
+        if @result1 == @result2 && @result2 == @result3
+        print `say "Congratulations you have won 5!"`
+        puts "Congratulations you have won 5!"
+        #Wallet.increase(wallet)
+        #@wallet = wallet
 
-    end
+        # push = (@result1 == @result2) || (@result2 == @result3) || (@result1 == @result3)
+        # push = (@result1 == @result2) || (@result2 == @result3) || (@result1 == @result3)
+        # if push == true
+        elsif @result1 == @result2 || @result2 == @result3 || @result1 == @result3
+        print `say "So close but at least you got your money back!"`
+        puts "So close but at least you got your money back!"
+    
+        else
+        print `say "You lost this round & your bet!"`
+        #Wallet.decrease(wallet)
+        #@money = wallet
+        end
+    
 end    
 welcome
 
